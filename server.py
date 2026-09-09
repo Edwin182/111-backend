0804e16c4753aad7c8fba9e4a96e9f6335fe4870
Edwin182 <edwinphone182@gmail.com>
2026-09-09 15:42:03 -0700

    assignment 2, add entries to the products and coupons table

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import sqlite3
 
 app = Flask(__name__)   # Instance of Flask 
@@ -17,6 +17,18 @@ def init_db():
         discount INTEGER NOT NULL      
     )
     """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL UNIQUE,
+        price REAL NOT NULL,
+        category TEXT NOT NULL,
+        image TEXT NOT NULL    
+    )    
+    """)
+
+
     connection.commit() # Save the changes to the D.B
     connection.close()  # Close the connection to the D.B 
 
@@ -24,6 +36,61 @@ def init_db():
 @app.get("/api/health")
 def health_check():
     return jsonify({"status": "ok"}), 200
+
+
+
+# ----------- PRODUCTS -------------
+
+# POST /api/products -> creation of a product to the DB
+@app.post("/api/products")
+def create_product():
+    # logic here
+    new_product = request.get_json()
+    print(new_product)
+
+    name = new_product["name"]
+    price = new_product["price"]
+    category = new_product["category"]
+    image = new_product["image"]
+
+    connection = sqlite3.connect(DB_NAME)    # opening the connection to the D.B
+    cursor = connection.cursor()    # cursor/tool help us to execute sql syntax (INSERT< SELCT.....) to the D.B 
+    cursor.execute("INSERT INTO products (name, price, category, image) VALUES (?, ?, ?, ?)",(name, price, category, image)
+)
+
+    connection.commit() # Save the changes to the D.B
+    connection.close()  # Close the connection to the D.B
+
+    return jsonify({
+        "success": True,
+        "message": "product created successfully"
+    }), 201
+
+
+# ---------- COUPONS ---------------
+@app.post("/api/coupons")
+def create_coupon():
+    # logic here
+    new_coupon = request.get_json()
+    print(new_coupon)
+    
+    code = new_coupon["code"]
+    discount = new_coupon["discount"]
+        
+    
+    connection = sqlite3.connect(DB_NAME)    # opening the connection to the D.B
+    cursor = connection.cursor()    # cursor/tool help us to execute sql syntax (INSERT< SELCT.....) to the D.B 
+    cursor.execute("INSERT INTO coupons (code, discount) VALUES (?, ?)",(code,discount)
+    )
+    
+    connection.commit() # Save the changes to the D.B
+    connection.close()  # Close the connection to the D.B
+    
+    return jsonify({
+        "success": True,
+        "message": "coupon created successfully"
+    }), 201
+
 
 init_db()
 app.run(debug=True)
