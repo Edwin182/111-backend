5c094bb1b0291460bae073a63b381027bf380b23
Edwin182 <edwinphone182@gmail.com>
2026-09-09 21:59:04 -0700

    assigmnet 3, adding logic to pick product and coupons by id

--- server.py
+++ server.py
@@ -67,20 +67,71 @@ def create_product():
     }), 201
 
 
+# GET http://127.0.0.1:5000/api/products    -> return all products 
+@app.get("/api/products")
+def get_products():
+
+    connection = sqlite3.connect(DB_NAME)   # Open the connection to the D.B
+    connection.row_factory = sqlite3.Row    # makes each row behaves like a dictionary
+    cursor = connection.cursor()            # execute sql syntax
+    cursor.execute("SELECT * FROM products")    
+    products_db = cursor.fetchall()         # retrieves all rows from the result of the query
+    connection.close()
+
+    products = []                   # list will store each product a a dictionary 
+    for product in products_db:
+        print(dict(product))        # convert the row object into a dictionary and print it
+        products.append(dict(product))  # 
+
+    return jsonify({
+        "success": True,
+        "message": "Products retrieved successfully",
+        "data": products
+        }), 200
+
+# GET http://127.0.0.1:5000/api/products/2  -> get a product by id
+@app.get("/api/products/<int:product_id>")
+def get_product_by_id(product_id):
+    #logic here
+    connection = sqlite3.connect(DB_NAME)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM products WHERE id =?", (product_id,))
+    product_db = cursor.fetchone()
+
+    if not product_db:
+        connection.close()
+        return jsonify({
+            "success": False,
+            "message": "product not found"
+        }), 404
+    
+    connection.close()
+    print(dict(product_db))
+    product = dict(product_db)
+
+    return jsonify({
+        "success": True,
+        "message": "product retrieved successfully",
+        "data": product
+    }), 200
+
+
 # ---------- COUPONS ---------------
 @app.post("/api/coupons")
 def create_coupon():
-    # logic here
+    # get the coupon from thunder client
     new_coupon = request.get_json()
     print(new_coupon)
-    
+
+    # create variables for coupon fields (code, discount)
     code = new_coupon["code"]
     discount = new_coupon["discount"]
         
     
     connection = sqlite3.connect(DB_NAME)    # opening the connection to the D.B
-    cursor = connection.cursor()    # cursor/tool help us to execute sql syntax (INSERT< SELCT.....) to the D.B 
-    cursor.execute("INSERT INTO coupons (code, discount) VALUES (?, ?)",(code,discount)
+    cursor = connection.cursor()    # cursor/tool help us to execute sql syntax (INSERT< SELECT.....) to the D.B 
+    cursor.execute("INSERT INTO coupons (code, discount) VALUES (?, ?)",(code, discount)    #execute sql (INSERT INTO)
     )
     
     connection.commit() # Save the changes to the D.B
@@ -90,6 +141,56 @@ def create_coupon():
         "success": True,
         "message": "coupon created successfully"
     }), 201
+
+#MINI-Challenge
+#GET http://127.0.0.1:5000/api/coupons -> return all the coupons 
+@app.get("/api/coupons")
+def get_coupons():
+
+    connection = sqlite3.connect(DB_NAME)   # Open the connection to the D.B
+    connection.row_factory = sqlite3.Row    # makes each row behaves like a dictionary
+    cursor = connection.cursor()            # execute sql syntax
+    cursor.execute("SELECT * FROM coupons")    
+    coupons_db = cursor.fetchall()         # retrieves all rows from the result of the query
+    connection.close()
+
+    coupons = []                   # list will store each product a a dictionary 
+    for coupon in coupons_db:
+        print(dict(coupon))        # convert the row object into a dictionary and print it
+        coupons.append(dict(coupon))  # 
+
+    return jsonify({
+        "success": True,
+        "message": "coupons retrieved successfully",
+        "data": coupons
+        }), 200
+
+# GET http://127.0.0.1:5000/api/coupons/2
+@app.get("/api/coupons/<int:coupon_id>")
+def get_coupon_by_id(coupon_id):
+    #logic here
+    connection = sqlite3.connect(DB_NAME)
+    connection.row_factory = sqlite3.Row
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM coupons WHERE id =?", (coupon_id,))
+    coupon_db = cursor.fetchone()
+
+    if not coupon_db:
+        connection.close()
+        return jsonify({
+            "success": False,
+            "message": "coupon not found"
+        }), 404
+    
+    connection.close()
+    print(dict(coupon_db))
+    coupon = dict(coupon_db)
+
+    return jsonify({
+        "success": True,
+        "message": "coupon retrieved successfully",
+        "data": coupon
+    }), 200
 
 
 init_db()
